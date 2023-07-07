5a8ee0895fe49863357d54c18e9d3d079f34b897
Mohamed Hashem <114192002+Mohamed-Hashem@users.noreply.github.com>
2023-07-07 15:46:16 +0300

    Time: 26 ms (73.90%), Space: 12 MB (14.43%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    int maxConsecutiveAnswersHelper(string answerKey, int k, char target){
+     int ans = -1,counter=0;
+        
+        for(int i =0,j=0; j <answerKey.size();j++ ){
+            
+            if (answerKey[j] == target)
+                counter++;
+            
+            if (counter > k){
+                
+                if (answerKey[i] == target)
+                    counter--;
+                i++;
+            }
+            
+            ans = max(ans,j - i +1);
+        }
+        
+        return ans;
+    }
+public:
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        
+        return max(maxConsecutiveAnswersHelper(answerKey,k,'T'), maxConsecutiveAnswersHelper(answerKey,k,'F'));
+    }
+};
