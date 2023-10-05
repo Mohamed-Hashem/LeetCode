1ad4c9aff9811e119e4ae991f5bf717f5f49f650
Mohamed Hashem <114192002+Mohamed-Hashem@users.noreply.github.com>
2023-10-05 20:51:11 +0300

    Time: 40 ms (99.29%), Space: 32.7 MB (29.71%) - LeetHub

---
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -0,0 +1,39 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("tune=native")
+static const auto fast = []()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    return 0;
+} ();
+
+class Solution {
+public:
+	vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+
+		sort(arr.begin(), arr.end());
+
+		vector<vector<int>> results;
+
+		int diff = INT_MAX, min_diff = INT_MAX;
+
+		for (int i = 1; i < arr.size(); i++) {
+            
+			diff = arr[i] - arr[i-1];
+            
+            if (diff == min_diff)
+			{
+				results.push_back({ arr[i - 1],arr[i] });
+                min_diff = diff;
+			} 
+            else if (diff < min_diff){
+                results.clear();
+                results.push_back({ arr[i - 1],arr[i] });
+                min_diff = diff;
+            }
+		}
+
+		return results;
+	}
+};
