590c0a527ac015a4a45307c0ad68aa3a51bf916a
Mohamed Hashem <114192002+Mohamed-Hashem@users.noreply.github.com>
2023-11-05 14:06:51 +0200

    Time: 3 ms (91.98%), Space: 13.9 MB (89.01%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-   	if (nums.size() == 0)
-		return { -1,-1 };
+        if (nums.size() == 0 )
+            return {-1,-1};
+        else if (nums.size() == 1 && nums[0] == target)
+		    return { 0,0 };
+        else {
+            
+int left = 0,right = nums.size() -1, mid;
+            
+            while (left <= right) {
+                mid = left + (right - left) / 2;
+                
+                if (target > nums[mid])
+                    left = mid + 1;
+                else if (target < nums[mid])
+                    right = mid - 1;
+                else {                   
+                    break;
+                }
+            }
+            
+            if (left >  right )
+                return {-1, -1};
+            else {
+ 
+                left = right = mid;
+                
+                while (left >= 0  && target == nums[left] )
+                    left--;
+            
+                while (right < nums.size() && target == nums[right])
+                    right++;
 
-	if (nums.size() == 1 && nums[0] == target)
-		return { 0,0 };
-
-	int low = 0, high = nums.size() - 1;
-	int mid = 0;
-
-	while (low <= high) {
-
-		mid = low + ((high - low) / 2);
-
-		if (target < nums[mid])
-			high = mid - 1;
-		else if (target > nums[mid])
-			low = mid + 1;
-		else
-			break;
-	}
-
-	if (low > high)
-		return { -1,-1 };
-
-	low = mid, high = mid;
-
-	while (low >= 0 && nums[low] == target )
-		low--;
-
-	while ( high < nums.size() && nums[high] == target)
-		high++;
-
-	return { low + 1,high - 1 };
+                return {left + 1, right - 1};    
+            }
+        }
     }
 };
